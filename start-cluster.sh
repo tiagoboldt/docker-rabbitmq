1ce728f7b20ceccb1dddd440e375d2f084adf078
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2013-12-06 16:58:23 +0000

    deleted makefile/dockerfile; updated start script

--- start-cluster.sh
+++ start-cluster.sh
@@ -13,15 +13,15 @@ test_image $VERSION
 for id in $(seq 1 $NODES); do
 
 	echo "Starting node $id"
-	hostname="cass$id"
+	hostname="rabbit$id"
 	ip=192.168.100.$id
 
 	# start container
-	if [[ $id == 1 ]]; then
-		ports="-p 127.0.0.1:19160:9160 -p 127.0.0.1:19042:9042"
-	else
-		ports=""
-	fi
+	# if [[ $id == 1 ]]; then
+	# 	ports="-p 127.0.0.1:19160:9160 -p 127.0.0.1:19042:9042"
+	# else
+	# 	ports=""
+	# fi
 	cid=$(sudo docker run -d -dns 127.0.0.1 -h $hostname $ports -t rabbitmq:$VERSION)
 
 	# Add network interface
